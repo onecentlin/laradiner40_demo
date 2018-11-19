6798ce5ac1a38e7234230224b65fcf69f852b774
Winnie Lin <onecentlin@gmail.com>
2018-11-20 00:14:53 +0800

    5-3: 使用 Mockery 產生假物件

--- tests/LogAnalyzerTest.php
+++ tests/LogAnalyzerTest.php
@@ -3,10 +3,17 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use Winnie\LaraDebut\ILogger;
 use Winnie\LaraDebut\LogAnalyzer;
+
+// 使用 Mockery 模擬框架
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 class LogAnalyzerTest extends TestCase
 {
+    use MockeryPHPUnitIntegration;
+
     /** @test */
     public function analyze_TooShortFileName_CallLogger()
     {
@@ -20,4 +27,19 @@ class LogAnalyzerTest extends TestCase
         // 把假物件當模擬物件來使用並驗證
         $this->assertContains("too short", $logger->lastError);
     }
+
+    /** @test */
+    public function analyze_TooShortFileName_CallLogger_UsingMockery()
+    {
+        // 建立模擬物件
+        $logger = m::mock(ILogger::class);
+        // 使用 Mockery API 來設定期望結果
+        $logger->shouldReceive('logError')->once()
+            ->with('Filename too short: a.txt');
+
+        $analyzer = new LogAnalyzer($logger);
+        $analyzer->minNameLength = 6;
+        $analyzer->analyze("a.txt");
+    }
+
 }
