bc7671d992527b57ff524dbfafe2d6680c5b7144
Winnie Lin <onecentlin@gmail.com>
2018-11-20 10:38:51 +0800

    5-6: 把 5-5 的測試轉換成 Mockery 的寫法

--- tests/LogAnalyzerTest.php
+++ tests/LogAnalyzerTest.php
@@ -5,6 +5,7 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use Winnie\LaraDebut\IFileNameRules;
 use Winnie\LaraDebut\ILogger;
+use Winnie\LaraDebut\IWebService;
 use Winnie\LaraDebut\LogAnalyzer;
 
 // 使用 Mockery 模擬框架
@@ -83,5 +84,33 @@ class LogAnalyzerTest extends TestCase
         $analyzer2->analyze($tooShortFileName);
 
         $this->assertContains("fake exception", $mockWebService->messageToWebService);
+    }
+
+    /** @test */
+    public function analyze_LoggerThrows_CallsWebService_UsingMockery()
+    {
+        $mockWebService = m::spy(IWebService::class);
+
+        $stubLogger = m::mock(ILogger::class);
+        $stubLogger->shouldReceive('logError')
+            ->with(
+                // 不論輸入任何參數, 模擬拋出例外的行為
+                m::on(function($message) {
+                    throw new \Exception("fake exception");
+                })
+            );
+
+        $analyzer2 = new LogAnalyzer2($stubLogger, $mockWebService);
+        $analyzer2->minNameLength = 8;
+        $tooShortFileName = "abc.ext";
+        $analyzer2->analyze($tooShortFileName);
+
+        // 確認 web 服務的模擬物件有被正確呼叫, 而且傳入的字串參數包含了 fake exception 的內容
+        $mockWebService->shouldHaveReceived('write')->once()
+            ->with(
+                m::on(function($message) {
+                    return strpos($message, 'fake exception') !== false;
+                })
+            );
     }
 }
